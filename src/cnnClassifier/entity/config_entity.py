a3c269279bd238de8e4856f72d7e3da30eb8971c
abhishekpatel16 <abhishekpatel0771@gmail.com>
2025-02-22 19:13:53 +0530

    model evaluation added

--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -40,6 +40,17 @@ class TrainingConfig:
 
 
 
+@dataclass(frozen=True)
+class EvaluationConfig:
+    path_of_model: Path
+    training_data: Path
+    all_params: dict
+    params_image_size: list
+    params_batch_size: int
+
+
+
+
 
 
 
